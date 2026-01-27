2178874ccd6986cbb1f340b56aaf35e7275fb2c6
mateusperego <mateusperego@yahoo.com.br>
2026-01-27 14:35:11 -0300

    feat: Ajustado migrations com proprietario na partida

--- database/migrations/2025_12_16_195001_games.php
+++ database/migrations/2025_12_16_195001_games.php
@@ -12,16 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('games', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('creator_id')->constrained('users')->cascadeOnDelete();
+            $table->id();            
 
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->enum('type', ['private', 'public'])->default('private');
             $table->dateTime('data_time');
 
-            $table->foreignId('club_id')->nullable()->constrained()->nullOnDelete();
-            $table->foreignId('court_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('club_id')->constrained('clubs')->cascadeOnDelete();
+            $table->foreignId('court_id')->constrained('courts')->cascadeOnDelete();
+            $table->foreignId('owner_player_id')->constrained('players')->cascadeOnDelete();
+
             $table->text('custom_location')->nullable();
 
             $table->unsignedTinyInteger('min_level')->nullable();
@@ -34,14 +35,12 @@ return new class extends Migration
             $table->decimal('price', 10, 2)->nullable();
             $table->decimal('cost_per_player', 10, 2)->nullable();
 
-
             $table->enum('game_type', ['casual', 'competitive', 'training'])->nullable()->default('casual');            
-            $table->integer('duration_minutes')->nullable()->default(90);            
-            
+            $table->integer('duration_minutes')->nullable()->default(90);                        
 
             $table->unsignedTinyInteger('team1_score')->nullable();
             $table->unsignedTinyInteger('team2_score')->nullable();
-            $table->unsignedTinyInteger('winner_team')->nullable();
+            $table->unsignedTinyInteger('winner_team')->nullable();           
 
             $table->timestamps();
 
