0da7ae84a7935aa6420aa45bf71098422e689ab8
mateusperego <mateusperego@yahoo.com.br>
2026-03-18 13:26:25 -0300

    feat: Tradução de mensagens no login

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Credenciais inválidas.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+];
