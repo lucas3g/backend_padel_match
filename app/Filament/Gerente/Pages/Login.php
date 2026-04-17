22a989ed496ace15ab056e061f229fcdc87c2d82
mateusperego <mateusperego@yahoo.com.br>
2026-04-17 14:14:18 -0300

    feat: Ajuste no login do gerente

--- app/Filament/Gerente/Pages/Login.php
+++ app/Filament/Gerente/Pages/Login.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Gerente\Pages;
 
 use Filament\Facades\Filament;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 
 class Login extends \Filament\Pages\Auth\Login
 {
+    public function authenticate(): LoginResponse
+    {
+        session()->forget('url.intended');
+
+        return parent::authenticate();
+    }
+
     protected function getRedirectUrl(): string
     {
         return Filament::getPanel('gerente')->getUrl();
